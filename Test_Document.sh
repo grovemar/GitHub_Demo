2450f9e0d00d0f05095e6a578a9d28d6477fc4f3
Hannah Richardson <richahan@woodward.cgrb.oregonstate.edu>
2019-08-06 17:13:01 -0700

    said 'hi' to Markas

--- Test_Document.sh
+++ Test_Document.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Hello folks! I'm a script on the master branch."
-
-#This script is too complicated to read. Adding comments."
+echo "Whatup!"
+#This script is too complicated to read. Adding comments.
